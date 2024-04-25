b6ef22332cb044b6f512c7c7d09ffb3dfe39ada9
Nikhil <nikhilwillworkwonders@gmail.com>
2024-04-25 14:34:46 -0400

    Add ticker input

--- api/frontend.py
+++ api/frontend.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 from nicegui import app, ui
 
-
 def init(fastapi_app: FastAPI) -> None:
     @ui.page('/')
     def show():
-        ui.label('Hello, FastAPI!')
+        ui.input(label="Enter a stock ticker:",
+                placeholder="e.g., AAPL",
+                on_change=lambda x: ticker.set_text("Searching for: " + x.value),
+                validation={"Input expects a ticker symbol": lambda value: len(value) == 4}  
+            )
+        ticker = ui.label()
+
         ui.dark_mode().bind_value(app.storage.user, 'dark_mode')
         ui.checkbox('dark mode').bind_value(app.storage.user, 'dark_mode')
 
